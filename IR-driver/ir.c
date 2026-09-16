1516fee54fa9076c1729b1fc3a3d35865321e2e2
Felipe Peres de Almeida <felipep.almeida15@gmail.com>
2026-09-15 22:04:44 -0400

    implemented basic usb driver interfaces for probing and disconnetion, also defined my private driver state as devtitans_ir_device.

--- IR-driver/ir.c
+++ IR-driver/ir.c
@@ -1,8 +1,8 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-#include <linux/kobject.h>
-#include <linux/sysfs.h>
+#include <linux/usb.h>
+#include <linux/slab.h>
 #include <linux/string.h>
 
 MODULE_LICENSE("GPL");                                    
@@ -10,13 +10,21 @@ MODULE_AUTHOR("Felipe Peres de Almeida");
 MODULE_DESCRIPTION("IR kernel module to connect with the IR hardware module for the HandsOn");  
 MODULE_VERSION("0.1");                                     
 
+#define VENDOR_ID  0x10C4  /* VendorID  do CP2102 */
+#define PRODUCT_ID 0xEA60  /* ProductID do CP2102 */
 
-char payload[512]; // TODO: check if the payload needs to be null terminated
+static const struct usb_device_id ir_table[] = {
+    { USB_DEVICE(VENDOR_ID, PRODUCT_ID) },
+    { }
+};
 
-static void ir_payload_show(struct kobject *kobject, struct kobj_attribute *attr, char* buf);
-static void ir_payload_store(void);
-static void ir_set_pulse_interval(float interval);
-static void set_payload_size(struct kobject *kobject, struct kobj_attribute *att, size_t size);
+MODULE_DEVICE_TABLE(usb, ir_table);
+
+static void ir_payload_show(struct device *dev, struct device_attribute *attr, char* buf);
+static void ir_payload_store(struct device* dev, struct device_attribute *attr, char* buf, size_t count);
+static void ir_probe(struct usb_interface *interface,
+                      const struct usb_device_id *id);
+static void ir_disconnect(struct usb_interface *interface);
 
 static ssize_t ir_payload_show(struct kobject *kobject, struct kobj_attribute *attr, char* buf){
     return sprintf(buf, "IR: %s\n", payload);
@@ -29,11 +37,44 @@ static ir_payload_store(struct kobject *kobject, struct kobj_attribute *attr, co
     return count;   
 }
 
-int init_module(void){
-    printk(KERN_INFO "IR: module loaded");
-    return 0;
+typedef struct ir_device {
+    struct usb_device *udev;
+    struct usb_interface *interface;
+    unsigned int carrier;
+} devtitans_ir_device;
+
+
+static int ir_probe(struct usb_interface *interface,
+                      const struct usb_device_id *id)
+{
+    struct devtitans_ir_device *ir;
+
+    ir = devm_kzalloc(&interface->dev, sizeof(*ir), GFP_KERNEL);
+    if (!ir)
+        return -ENOMEM;
+
+    ir->udev = interface_to_usbdev(interface);
+    ir->interface = interface;
+    ir->carrier = 38000;
+
+    usb_set_intfdata(interface, ir);
+
+    dev_set_drvdata(&interface->dev, ir);
+
+    return device_create_file(&interface->dev, &dev_attr_carrier);
 }
 
-void cleanup_module(void){
-    printk(KERN_INFO "IR: module unloaded");
+static void myir_disconnect(struct usb_interface *interface)
+{
+    device_remove_file(&interface->dev, &dev_attr_carrier);
+    usb_set_intfdata(interface, NULL);
 }
+
+static struct usb_driver devtitans_ir_driver = {
+    .name = "devtitans_ir_driver",
+    .probe = ir_probe,
+    .disconnect = ir_disconnect,
+    .id_table = ir_table
+}
+
+module_usb_driver(devtitans_ir_driver);
